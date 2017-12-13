6dd07c039d5fe2089e4b987185b4996e9fb80325
Tiago Reis <tigasmcl@gmail.com>
2017-12-13 22:08:34 +0000

    És um conas
    Continuas a ser conas

--- src/FoxCona.java
+++ src/FoxCona.java
@@ -3,8 +3,9 @@ public class FoxCona {
 	
 	public static void main (String[] args){
 		while (true){
-			System.out.println("O Fox é um conas do caralho");
+			System.out.println("O Fox Ã© um conas do caralho");
 			System.out.println("E deixou o pc desbloqueado no neei :)");
+			System.out.println("Passado 3 anos, continua a ser conas");
 		}
 	}
 	
