0b49dc5f7061a5169b21336a9d6b495244277e6b
tigas <tigas@DESKTOP-E71FK07>
2020-05-05 23:39:48 +0100

    Upgrade na syntax para estilo VB. Adição de uma variavel com o nome totalmente enganador

--- src/FoxCona.java
+++ src/FoxCona.java
@@ -1,11 +1,19 @@
 
-public class FoxCona {
+public class FoxCona
+{
+	static int usado;
+	static boolean ativo;
 	
-	public static void main (String[] args){
-		while (true){
+	public FoxCona() 
+	{	
+	}
+	
+	public static void main (String[] args)
+	{
+		while (ativo)
+		{
 			System.out.println("O Fox é um conas do caralho");
 			System.out.println("E deixou o pc desbloqueado no neei :)");
 		}
 	}
-	
 }
